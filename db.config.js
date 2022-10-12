62fd4867b4a44fa2b07768c0043939ec84a84eae
Gabriel U <ullmanng162@gmail.com>
2022-10-12 11:06:13 -0400

    fix dbconfig

--- db.config.js
+++ db.config.js
@@ -12,18 +12,3 @@ module.exports = {
     idle: 10000
   }
 };
-
-/*
-module.exports = {
-  HOST: "mysql15-farm10.kinghost.net",
-  USER: "ghhomologacao41",
-  PASSWORD: "higigo2019",
-  DB: "ghhomologacao41",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};*/
